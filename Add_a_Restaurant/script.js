9b9f34ec78b2c0cf47f97cd8413cb994d463922d
Megalodon <gabjagmaxthi@gmail.com>
2024-05-29 23:08:47 +0200

    Default Add

--- Add_a_Restaurant/script.js
+++ Add_a_Restaurant/script.js
@@ -3,7 +3,7 @@ passWord = "Ax2346Zb";
 const connect = {
     check: function() {
         if (document.getElementById('code').value == passWord) {
-            /*listRem = ['fieldSetterCode','ButtonCodeSend'];
+            listRem = ['fieldSetterCode','ButtonCodeSend'];
             listRem.forEach(el => {document.getElementById(el).style = "opacity: 0;"})
             setTimeout(() => {
                 listRem.forEach(el => {document.getElementById(el).style = "display: none;";})
@@ -15,11 +15,12 @@ const connect = {
             setTimeout(() => {
                 document.getElementById('authenticator').style = "opacity: 0;";
                 document.getElementById('containator').classList.remove('blur');
-            }, 2500);*/
+            }, 2500);
             setTimeout(() => {
-                document.getElementById('authenticator').style = "display: none";
+                document.getElementById('authenticator').style = "display: none;";
                 document.getElementById('containator').classList.remove('blur');
-            }, 0);
+                document.getElementById('containator').style = "z-index: 9999;";
+            }, 3000);
         } else {
             document.getElementById('fieldSetterCode').style = "border: 2px solid rgba(255, 70, 70, 0.6);";
         }
