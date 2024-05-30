a02f18b37e620d9644fb589bacad2d52b31ad685
Megalodon <gabjagmaxthi@gmail.com>
2024-05-30 22:01:47 +0200

    Default Add

--- Add_a_Restaurant/script.js
+++ Add_a_Restaurant/script.js
@@ -1,13 +1,12 @@
+var stuck = false;
+var endRefuse;
 const connect = {
     check: function() {
         fetch("https://raw.githubusercontent.com/projectMTGJ/StrasCroute/master/Add_a_Restaurant/token.json")
             .then(response => response.json())
             .then(data => {
-                if (document.getElementById('code').value == data.passWord) {
-                    this.open();
-                } else {
-                    document.getElementById('fieldSetterCode').style = "border: 2px solid rgba(255, 70, 70, 0.6);";
-                }
+                if (document.getElementById('code').value == data.passWord) this.open();
+                else this.refuse();
             })
             .catch(error => console.error('Error fetching the token:', error));
     },
@@ -20,6 +19,7 @@ const connect = {
             .catch(error => alert("Il y a un problème veuillez contacter l'Administrateur"+error));
     },
     open: function(start=400) {
+        endRefuse && clearTimeout(endRefuse);
         ['fieldSetterCode','ButtonCodeSend'].forEach(el => {document.getElementById(el).style = "opacity: 0;"})
         setTimeout(() => {
             document.getElementById('personImage').style = "transform: translateY(32%);";
@@ -29,15 +29,26 @@ const connect = {
             document.getElementById('authcontainer').style = "box-shadow: 0 0 400px rgb(61, 205, 25); background-color: rgb(61, 205, 25);";
             document.getElementById('loadericon').style = "opacity: 1; height: 80px;";
         }, start+=200);
+        if (stuck === true) return;
         setTimeout(() => {
             document.getElementById('authenticator').style = "opacity: 0; transform: scale(1.5);";
             document.getElementById('containator').classList.remove('blur');
-        }, start+=2000);
+        }, start+=1500);
         setTimeout(() => {
             document.getElementById('authenticator').style = "display: none;";
             document.getElementById('containator').classList.remove('blur');
             document.getElementById('containator').style = "z-index: 9999;";
         }, start+=400);
+    },
+    refuse: function() {
+        document.getElementById('fieldSetterCode').style = "border: 2px solid rgba(255, 70, 70, 0.6);";
+        document.getElementById('authcontainer').style = "box-shadow: 0 0 400px rgb(255, 70, 70); background-color: rgb(155, 70, 70);";
+        document.getElementById('personImage').style = "border: 4px solid rgb(255, 70, 70); border-radius: 100px;";
+        endRefuse = setTimeout(() => {
+            document.getElementById('fieldSetterCode').style = "border: 2px solid aliceblue;";
+            document.getElementById('authcontainer').style = "box-shadow: 0 0 40px rgb(255, 128, 0); background-color: rgb(87, 87, 87);";
+            document.getElementById('personImage').style = "border: 4px solid aliceblue; border-radius: 50px;";
+        }, 2000);
     },
     lock: function() {
         location.reload()
