d75427dce19ceab019eaa838d05266a24e94d65f
Megalodon <gabjagmaxthi@gmail.com>
2024-06-01 18:20:01 +0200

    Default Add

--- ManageRestaurants/add/script.js
+++ ManageRestaurants/add/script.js
@@ -2,12 +2,18 @@ var checkAuthWhenConnect = function() {
     const currentUrl = window.location.href;
     const currentUrlObj = new URL(currentUrl);
     const currentParams = new URLSearchParams(currentUrlObj.search);
-    const code = currentParams.get('auth');
     fetch("https://raw.githubusercontent.com/projectMTGJ/StrasCroute/master/ManageRestaurants/token.json")
         .then(response => response.json())
         .then(data => {
-            if (code == data.key) return;
-            else window.close();
+            if (currentParams.get('auth') == data.key) return;
+            else {
+                //alert(data.key, currentParams.get('auth'));
+                document.getElementById('containMain').style = "filter: blur(50px);"
+                setTimeout(() => {
+                    //window.close();
+                }, 5000);
+            }
         })
         .catch(error => alert("Il y a un problème veuillez contacter l'Administrateur"+error));
 }
+
