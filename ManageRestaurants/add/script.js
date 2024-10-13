b011d44ed295531e4d4d6d7bed0a132f209645f2
Gabriel JAGUENEAU <gabjagmaxthi@gmail.com>
2024-10-13 20:28:06 +0200

    finished docu, beta on phones

--- ManageRestaurants/add/script.js
+++ ManageRestaurants/add/script.js
@@ -22,3 +22,36 @@ var checkAuthWhenConnect = function() {
 }
 
 //const alwaysCheck = setInterval(() => checkAuthWhenConnect(), 10000);
+
+const manage = {
+    addCompo: function(origin) {
+        const doc = document.getElementById(`${origin}Liste`);
+        const num = doc.children.length;
+        if (num >= 7) return;
+        const card = document.createElement('fieldset');
+        card.classList.add('card');
+        card.id = `${num}${origin}`
+        card.innerHTML = `
+        <legend>${origin} n°${num} 
+            <img onclick="manage.removeCompo('${num}${origin}', '${origin}')" src="https://raw.githubusercontent.com/projectMTGJ/StrasCroute/refs/heads/master/images/delete.png" alt="delete">
+        </legend>
+        <input type="file" name="${num}${origin}image" id="${num}${origin}image">
+        <input type="text" name="${num}${origin}name" id="${num}${origin}name" placeholder="Nom du plat">
+        <input type="text" name="${num}${origin}prix" id="${num}${origin}prix" placeholder="Prix du plat">
+        <fieldset class="note" id="${num}${origin}note">
+            <legend>Qualité globale</legend>
+            <div class="top-values-range"><div class="val">0</div><div class="val">1</div><div class="val">2</div><div class="val">3</div><div class="val">4</div><div class="val">5</div></div>
+            <input type="range" min="0" max="50" id="${num}${origin}note" value="0">
+        </fieldset>
+        `;
+        doc.appendChild(card);
+    },
+    removeCompo: function(id, origin) {
+        const doc = document.getElementById(`${origin}Liste`);
+        const card = document.getElementById(id);
+        doc.removeChild(card);
+    },
+    updateCount: function(id) {
+        document.getElementById(`${id}value`).innerHTML = document.getElementById(id).value/10;
+    }
+}
